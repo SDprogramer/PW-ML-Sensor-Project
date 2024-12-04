aa9fe23536d5deb10cb6dd73331ef609a2de0719
SDprogramer <soumyadeep.dey162003@gmail.com>
2024-12-04 09:10:06 +0530

    Project run successfully

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 from flask import Flask, render_template, jsonify, request, send_file
 from src.exception import CustomException
 from src.logger import logging as lg
-import os,sys
-
+import os, sys
 
 from src.pipeline.train_pipeline import TrainingPipeline
 from src.pipeline.predict_pipeline import PredictionPipeline
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "Welcome to my application"
-
-
-
 
 @app.route("/train")
 def train_route():
@@ -24,46 +18,31 @@ def train_route():
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
 
-
         return "Training Completed."
-
-
+    
     except Exception as e:
         raise CustomException(e,sys)
 
-
-@app.route('/predict', methods=['POST', 'GET'])
-def upload():
-   
+@app.route('/predict', methods = ['POST', 'GET'])
+def upload():   
     try:
-
-
-
-
         if request.method == 'POST':
-            # it is a object of prediction pipeline
+            # It is a object of prediction pipeline
             prediction_pipeline = PredictionPipeline(request)
            
-            #now we are running this run pipeline method
+            # Now we are running this run pipeline method
             prediction_file_detail = prediction_pipeline.run_pipeline()
 
-
-            lg.info("prediction completed. Downloading prediction file.")
+            lg.info("Prediction completed. Downloading prediction file.")
+            
             return send_file(prediction_file_detail.prediction_file_path,
-                            download_name= prediction_file_detail.prediction_file_name,
-                            as_attachment= True)
-
-
-
-
+                            download_name = prediction_file_detail.prediction_file_name,
+                            as_attachment = True)
         else:
             return render_template('upload_file.html')
+        
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
    
-
-
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug= True)
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
